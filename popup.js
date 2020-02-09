ec9d7dcc4b33d2016607bde90ac70992c6bc4a9d
Berthelmaster <berthel0802@gmail.com>
2020-02-09 20:10:22 +0100

    A bit of polish and optimization

--- popup.js
+++ popup.js
@@ -2,19 +2,16 @@ Cookies.set('gg', 'ggg');
 Cookies.set('tests', 'testtest');
 function getall() {
 
-    var renderId = document.getElementById("output");
     var cookies = Cookies.get();
     var array = new Array();
-    var iterate;
 
     for(var prop in cookies)
     {
-        iterate = prop;
-        if(iterate == '_fbp'){array.push('Facebook')} //facebook
-        if(iterate == '_gid'){array.push('Google')} //Google
-        if(iterate == 'gg'){array.push('Test')} //Test
-        if(iterate == 'tests'){array.push('Wat')} //TestV2
-        console.log(iterate)
+        if(prop == '_fbp'){array.push('Facebook'); continue;} //facebook
+        if(prop == '_gid'){array.push('Google'); continue;} //Google
+        if(prop == 'gg'){array.push('Test'); continue;} //Test
+        if(prop == 'tests'){array.push('Wat'); continue;} //TestV2
+        console.log(prop)
     }
 
 
@@ -23,6 +20,6 @@ function getall() {
     for(var html in array)
     {
         var output = array[html]
-        $('#output').append(`<div>${array[html]}</div>`);
+        $('#output').append(`<p>${output}</p>`);
     }
 }
