95f836851d14c0f3636dd9d7fe9ab66716d6d401
Berthelmaster <berthel0802@gmail.com>
2020-02-09 19:40:21 +0100

    jquery and other libraries added, should try to release tomorrow with updated ui design

--- popup.js
+++ popup.js
@@ -1,3 +1,28 @@
-var getCookies = function () {
-    return document.cookie;
+Cookies.set('gg', 'ggg');
+Cookies.set('tests', 'testtest');
+function getall() {
+
+    var renderId = document.getElementById("output");
+    var cookies = Cookies.get();
+    var array = new Array();
+    var iterate;
+
+    for(var prop in cookies)
+    {
+        iterate = prop;
+        if(iterate == '_fbp'){array.push('Facebook')} //facebook
+        if(iterate == '_gid'){array.push('Google')} //Google
+        if(iterate == 'gg'){array.push('Test')} //Test
+        if(iterate == 'tests'){array.push('Wat')} //TestV2
+        console.log(iterate)
+    }
+
+
+    console.log(array);
+
+    for(var html in array)
+    {
+        var output = array[html]
+        $('#output').append(`<div>${array[html]}</div>`);
+    }
 }
